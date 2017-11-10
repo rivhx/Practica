0f77aa6a8f8888941a8212f561914ee2095e0864
rivh <ricardo.valverde.hernandez@gmail.com>
2017-11-10 16:45:54 -0600

    method get image v2

--- app/src/main/java/com/rivh/practica/MainActivity.java
+++ app/src/main/java/com/rivh/practica/MainActivity.java
@@ -1,11 +1,12 @@
 package com.rivh.practica;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,15 +15,15 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
 
-import org.json.JSONObject;
-import org.w3c.dom.Text;
 
 import java.util.HashMap;
 import java.util.Map;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText mEditTextApellido;
     private Button mButtonPost;
     private Button mButtonGet;
+    private Button mButtonGetImage;
+    private ImageView mImageResponse;
 
 
     private RequestQueue mRequestQueue;
@@ -47,12 +50,21 @@ public class MainActivity extends AppCompatActivity {
         mEditTextApellido = (EditText) findViewById(R.id.main_activity_txtApellido);
         mButtonPost = (Button) findViewById(R.id.main_activity_button_post);
         mButtonGet = (Button) findViewById(R.id.main_activity_button_get);
+        mButtonGetImage = (Button) findViewById(R.id.main_activity_button_get_image);
+        mImageResponse = (ImageView) findViewById(R.id.main_activity_image);
 
 
         mButtonGet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 sendGet();
+            }
+        });
+
+        mButtonGetImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendGetImage();
             }
         });
 
@@ -77,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
     void sendGet() {
         // Instantiate the RequestQueue.
         mRequestQueue = Volley.newRequestQueue(this);
-        String url = "http://172.18.116.71:8082/";
+        String url = "http://192.168.100.11:8082/";
 
 // Request a string response from the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
@@ -87,21 +99,31 @@ public class MainActivity extends AppCompatActivity {
                         // Display the first 500 characters of the response string.
                         mTextView.setText("Response is: " + response.toString());
                     }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                mTextView.setText("That didn't work!");
-            }
-        });
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        mTextView.setText("That didn't work!");
+                    }
+                });
 // Add the request to the RequestQueue.
         mRequestQueue.add(stringRequest);
 
 
     }
 
+    void sendGetImage() {
+
+        String url = "http://192.168.100.11:8082/logo";
+
+        Picasso.with(this.getApplicationContext()).load(url).into(mImageResponse);
+
+
+    }
+
     void sendPost() {
 
-        String url = "http://172.18.116.71:8082/estudiantes";
+        String url = "http://192.168.100.11:8082/estudiantes";
 
         StringRequest StringReq = new StringRequest(Request.Method.POST, url,
                 new Response.Listener<String>() {
